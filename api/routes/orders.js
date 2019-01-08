423d920395502a26c32ec4cabbe2f94d632d5bb0
Abraham <hayrapetyanabrahamgmail.com>
2019-01-09 02:06:40 +0400

    Handling Errors and Parsing the Body and Handling CORS

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+//Handle incoming GET request to /orders
 router.get('/', (req, res, next) => {
     res.status(200).json({
         message: 'Order were fetched'
@@ -8,16 +9,39 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
+    const order = {
+        productId: req.body.productId,
+        quantity: req.body.quantity
+    };
     res.status(201).json({
-        message: 'Order was created'
+        message: 'Order was created',
+        order: order
     });
 });
 
-router.get('/:orderId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Order details',
-        orderId: req.params.orderId
-    });
+// router.get('/:orderId', (req, res, next) => {
+//     res.status(200).json({
+//         message: 'Order details',
+//         orderId: req.params.orderId
+//     });
+// });
+
+
+router.get('/:name', (req, res, next) => {
+    const name = req.params.name;
+    if (name == 'Hayrapet') {
+        res.status(200).json({
+            tariq: 31,
+            id: 1254,
+            name: name
+        });
+    } else {
+        res.status(200).json({
+            tariq: 27,
+            id: 1255,
+            name: 'Abraham'
+        })
+    }
 });
 
 router.delete('/:orderId', (req, res, next) => {
--- api/routes/products.js
+++ api/routes/products.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+const mongoose = require('mongoose');
+
+const Product = require('../models/product');
+
 router.get('/', (req, res, next) => {
     res.status(200).json({
         message: 'Handeling Get Request to /prouducts '
@@ -8,8 +12,26 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
+
+    const product = {
+        name: req.body.name,
+        price: req.body.price
+    };
+
+
+    // const product = new Product({
+    //     _id: new mongoose.Types.ObjectId,
+    //     name: req.body.name,
+    //     price: req.body.price
+
+    // });
+    // product.save().then(result => {
+    //     console.log(result);
+    // }).catch(err => console.log(err));
+
     res.status(201).json({
-        message: 'Handeling POST Request to /prouducts '
+        message: 'Handeling POST Request to /prouducts ',
+        createdProduct: product
     });
 });
 
