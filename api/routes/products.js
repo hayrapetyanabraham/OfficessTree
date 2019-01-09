3a253d34fb852126e1b418d27d1d48c12f78863e
Abraham <hayrapetyanabrahamgmail.com>
2019-01-10 01:40:07 +0400

    Added DB MongoDB and Mongoose

--- api/routes/products.js
+++ api/routes/products.js
@@ -6,63 +6,103 @@ const mongoose = require('mongoose');
 const Product = require('../models/product');
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Handeling Get Request to /prouducts '
-    });
+    Product.find()
+        .exec()
+        .then(docs => {
+            console.log(docs);
+            res.status(200).json(docs);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 
 router.post('/', (req, res, next) => {
 
-    const product = {
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
         price: req.body.price
-    };
-
-
-    // const product = new Product({
-    //     _id: new mongoose.Types.ObjectId,
-    //     name: req.body.name,
-    //     price: req.body.price
-
-    // });
-    // product.save().then(result => {
-    //     console.log(result);
-    // }).catch(err => console.log(err));
-
-    res.status(201).json({
-        message: 'Handeling POST Request to /prouducts ',
-        createdProduct: product
     });
-});
 
-router.get('/:prouductId', (req, res, next) => {
-    const id = req.params.prouductId;
-    if (id == 'special') {
-        res.status(200).json({
-            message: 'You descovered the special ID',
-            id: id
+    product.save()
+        .then(result => {
+            console.log(result);
+            res.status(201).json({
+                message: 'Handeling POST Request to /prouducts ',
+                createdProduct: product
+            });
+        }).catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+
         });
-    } else {
-        res.status(200).json({
-            message: 'You passed an ID'
+
+});
+
+router.get('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    Product.findById(id)
+        .exec()
+        .then(doc => {
+            console.log("From database", doc);
+            if (doc) {
+                res.status(200).json(doc);
+            } else {
+                res
+                    .status(404)
+                    .json({ message: "No valid entry found for provided ID" });
+            }
         })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: err });
+        });
+
+});
+
+
+router.patch('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    const updateOps = {};
+    for (const ops of req.body) {
+        updateOps[ops.propName] = ops.value;
     }
+    // Product.update({ _id: id }, { $set: { name: req.body.newName, price: req.body.newPrice } });
+    Product.update({ _id: id }, { $set: updateOps })
+        .exec()
+        .then(result => {
+            console.log(result);
+            res.status(200).json(result);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+
 
 });
 
-
-router.patch('/:prouductId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Updated product!'
-    });
-
-});
-
-router.delete('/:prouductId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Deleted product!'
-    });
-
+router.delete('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    Product.remove({ _id: id })
+        .exec()
+        .then(result => {
+            res.status(200).json(result);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 
 
--- app.js
+++ app.js
@@ -2,14 +2,25 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser')
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
 const prouductRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
-// mongoose.connect('mongodb://Hayrapetyan:' + process.env.MONGO_ATLAS_PW + '@cluster0-shard-00-00-jkvb2.mongodb.net:27017,cluster0-shard-00-01-jkvb2.mongodb.net:27017,cluster0-shard-00-02-jkvb2.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true', {
-//     useMongoClient: true
-// })
+// mongoose.connect('mongodb://Hayrapetyan:' +
+//     process.env.MONGO_ATLAS_PW +
+//     '@cluster0-shard-00-00-jkvb2.mongodb.net:27017,cluster0-shard-00-01-jkvb2.mongodb.net:27017,cluster0-shard-00-02-jkvb2.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',
+//     {
+//         useMongoClient: true
+//     }
+// );
+
+
+mongoose.connect('mongodb+srv://Hayrapetyan:'+process.env.MONGO_ATLAS_PW +'@cluster0-jkvb2.mongodb.net/test?retryWrites=true',
+    {
+        useNewUrlParser: true
+    }
+);
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({
