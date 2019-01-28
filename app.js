986f77c098cdd238011f0dd4d608318c09752dac
Abraham <hayrapetyanabrahamgmail.com>
2019-01-28 21:55:21 +0400

    Uploading an Image

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ mongoose.connect('mongodb+srv://Hayrapetyan:'+process.env.MONGO_ATLAS_PW +'@clus
 mongoose.Promise = global.Promise;
 
 app.use(morgan('dev'));
+app.use('/uploads', express.static('uploads'));
 app.use(bodyParser.urlencoded({
     extended: false
 }));
